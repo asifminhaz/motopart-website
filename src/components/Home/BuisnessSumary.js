24d56592e5dac454279ea075be3e5d7579c86364
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-22 15:13:14 +0600

    navber added

---
+++ src/components/Home/BuisnessSumary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BuisnessSumary = () => {
+          return (
+                    <div>
+                              
+                    </div>
+          );
+};
+
+export default BuisnessSumary;
