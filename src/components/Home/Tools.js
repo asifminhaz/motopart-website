238ddc43216344756adf6d36f1533260ee25dcc6
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-23 01:21:06 +0600

    show tools on home

--- src/components/Home/Tools.js
+++ src/components/Home/Tools.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
 
 const Tools = () => {
+        const [tools, setTools] = useState([])
+
+        useEffect(() => {
+                fetch('http://localhost:5000/tool')
+                .then(res => res.json())
+                .then(data => setTools(data))
+        },[])
           return (
                     <div>
-                            <h2>this is tools</h2>  
+                            <h2 className='text-3xl text-center m-10 font-bold'>Tools Collection</h2>
+                            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                            {
+                                    tools.slice(0, 6).map(tool => <Tool
+                                    key={tool._id}
+                                    tool={tool}
+                                    ></Tool>)
+                            }  
+                            </div>
                     </div>
           );
 };
