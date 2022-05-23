aed24be95d52553e69ad7cc911bfd8367c172245
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-23 15:19:06 +0600

    add business summery

--- src/components/Home/BuisnessSumary.js
+++ src/components/Home/BuisnessSumary.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 const BuisnessSumary = () => {
           return (
-                    <div>
-                              
+                    <div className='text-center justify-items-center'>
+                             <h1 className='text-center font-3xl font-bold m-10'>Business Summary</h1> 
+                             <div class="stats stats-vertical lg:stats-horizontal shadow ">
+  
+  <div class="stat">
+    <div class="stat-title font-bold">Company Revenue</div>
+    <div class="stat-value">31K</div>
+    <div class="stat-desc">Jan 1st - Feb 1st</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-title font-bold">Clients</div>
+    <div class="stat-value">4,200</div>
+    <div class="stat-desc">↗︎ 400 (22%)</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-title font-bold">Supply Countries</div>
+    <div class="stat-value">30</div>
+    <div class="stat-desc">↘︎ 90 (14%)</div>
+  </div>
+  
+</div>
                     </div>
           );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import BuisnessSumary from './BuisnessSumary';
 import Tools from './Tools';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
                     <div>
                            <Banner></Banner>
                            <Tools></Tools>
+                           <BuisnessSumary></BuisnessSumary>
                     </div>
           );
 };
