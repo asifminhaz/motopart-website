691377d978d050e5e7fdacd73059a17e19de1b17
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-27 01:08:57 +0600

    add blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,8 +3,69 @@ import React from 'react';
 const Blogs = () => {
           return (
                     <div>
-                       <h2>this is blogs</h2>       
-                    </div>
+                       <h2 className='text-center font-bold text-4xl'>This is Blogs</h2>      
+                       <div class="card w-96 bg-base-100 shadow-xl">
+  
+  <div class="card-body">
+    <h2 class="card-title">
+     
+      <div class="text-center">How will you improve the performance of a React Application??</div>
+    </h2>
+
+    <p>Keeping component state local where necessary.
+Memoizing React components to prevent unnecessary re-renders.
+Code-splitting in React using dynamic import()
+Windowing or list virtualization in React.
+Lazy loading images in React.</p>
+    
+    </div>
+  </div>
+     <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">
+     
+      <div class="text-center">What are the different ways to manage a state in a React application??</div>
+    </h2>
+
+    <li>1.Local state..</li>
+    <li>2.Global state.</li>
+    <li>3. Server state.</li>
+    <li>4.URL state</li>
+    
+    </div>
+  </div>
+     <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">
+     
+      <div class="text-center">You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name??</div>
+    </h2>
+   <p> I can use filter or inclueds for find the find the products by name .its very simple function  </p>
+    
+    </div>
+  </div>
+     <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">
+     
+      <div class="text-center">How does prototypical inheritance work???</div>
+    </h2>
+    <p>Simply put, prototypical inheritance refers to the ability to access object properties from another object. We use a JavaScript prototype to add new properties and methods to an existing object constructor. We can then essentially tell our JS code to inherit properties from a prototype</p>
+    
+    </div>
+  </div>
+     <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">
+     
+      <div class="text-center">What is a unit test? Why should write unit tests???</div>
+    </h2>
+    <p>Unit testing is a software development process in which the smallest testable parts of an application, called units, are individually and independently scrutinized for proper operation.One of the benefits of unit tests is that they isolate a function, class or method and only test that piece of code. Higher quality individual components create overall system resiliency. Thus, the result is reliable code. Unit tests also change the nature of the debugging process</p>
+    
+    </div>
+  </div>
+</div> 
+     
           );
 };
 
--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -4,6 +4,51 @@ const Profile = () => {
           return (
                     <div>
                               <h3>welcome to your profile</h3>
+                              <div className="hero">
+                                        <form >
+                                        <div className="hero-content flex-col lg:flex-row-reverse">
+                                  <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                                <div className="card-body">
+                                      <div className="form-control">
+                                         <label className="label">
+                                             </label>
+                                          <input  type="text" placeholder="your name" className="input input-bordered" required />
+                                       </div>
+                                      <div className="form-control">
+                                         <label className="label">
+                                             </label>
+                                          <input  type="text" placeholder="email" className="input input-bordered" required />
+                                       </div>
+                                      <div className="form-control">
+                                         <label className="label">
+                                             </label>
+                                          <input  type="text" placeholder="Education" className="input input-bordered" required />
+                                       </div>
+                                      <div className="form-control">
+                                         <label className="label">
+                                             </label>
+                                          <input  type="number" placeholder="phone number" className="input input-bordered" required />
+                                       </div>
+                                      <div className="form-control">
+                                         <label className="label">
+                                             </label>
+                                          <input  type="text" placeholder="LinkeDin " className="input input-bordered" required />
+                                       </div>
+                                      <div className="form-control">
+                                         <label className="label">
+                                             </label>
+                                          <input  type="text" placeholder="Address" className="input input-bordered" required />
+                                       </div>
+                                          <div className="form-control mt-6">
+                                      <button className="btn btn-primary">Submit</button>
+                                      
+                                              
+                                           </div>
+                                          </div>
+                                          </div>
+                                        </div>
+                                        </form>
+                              </div>
                     </div>
           );
 };
